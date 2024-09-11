3e2e8c7fa23744b0fb03be45ecea97c95bcaaed9
Tạ Đăng Lâm <tadanglma93@gmail.com>
2024-09-11 18:26:41 +0700

    feat: build Movie model

---
+++ src/main/java/com/example/clothing_store/Movie.java
@@ -0,0 +1,39 @@
+package com.example.clothing_store;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import java.util.List;
+
+@Document(collation = "review")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Movie {
+
+    @Id
+    private ObjectId id;
+
+    private String imdbId;
+
+    private String title;
+
+    private String releaseDate;
+
+    private String trailerLink;
+
+    private String poster;
+
+    private List<String> genners;
+
+    private List<String> backdrops;
+
+    @DBRef
+    private List<Review> reviewId;
+}
